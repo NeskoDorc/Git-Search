ce7c5564fe32394c4cfe412689a51e2b3ed5679c
NeskoDorc <nebojsa85.milosevic@gmail.comm>
2021-11-21 15:41:26 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//Search Input
+
+const searchUser = document.getElementById('searchUser')
+
+//Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+
+    // Get input text 
+    const userText = e.target.value
+
+    if (userText !== '') {
+        console.log(userText)
+    }
+
+})
